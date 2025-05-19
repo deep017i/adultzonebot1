de8fa45f96eb9274d64a212bf7ed1c4dac49bbcf
Deep Coder <120001510+deep017i@users.noreply.github.com>
2025-05-19 22:08:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,7 +3,7 @@ from typing import List
 
 API_ID = int(os.getenv("API_ID", "27339145"))
 API_HASH = os.getenv("API_HASH", "e620f86919c34496729285dd2a6d35e6")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7855384569:AAH1rGlzgE4kUwziLmtZ9m8LbLu3fNF2oqo")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7579290702:AAHqYG6mrEN8Gnim6txjyAMo7GM8ynzl2SE")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://starcinebot:mkooaa@werdeveloper.vxfam.mongodb.net/?retryWrites=true&w=majority&appName=werdeveloper")
 DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002474668306"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7025449786"))
