3f0353f5831634988a5c86356894fac2d4e49da9
Deep Coder <120001510+deep017i@users.noreply.github.com>
2025-05-19 22:06:46 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, Response
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+def show_github_html():
+    response = requests.get('https://raw.githubusercontent.com/Dypixx/live/main/index.html')
+    return Response(response.text, mimetype='text/html') if response.status_code == 200 else ("Bot is LIVE", 500)
+
+if __name__ == "__main__":
+    app.run()
